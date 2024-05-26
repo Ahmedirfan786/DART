2e11a5faf16824eaab9c6bce092e1f1fc99437e4
Ahmed Irfan <alphastarx17@gmail.com>
2024-05-26 22:29:33 +0500

    Added file Lessons->lesson1

---
+++ Lessons/ls1_Introduction to Dart.dart
@@ -0,0 +1,59 @@
+// INTRODUCTION TO DART
+
+
+// ❔ What is Dart
+// 💡=>Dart is a programming language developed by Google, primarily used for web, server, and mobile application development. It offers compilation options: "Ahead of Time" (AOT) into native code or "Just in Time" (JIT) into bytecode for the Dart Virtual Machine (VM).
+// Notably, Dart powers Flutter, Google's UI toolkit for building natively compiled apps across mobile, web, and desktop platforms from a single codebase. This integration simplifies cross-platform development, enhancing productivity and performance, and making it a preferred choice for diverse project types.
+
+
+// ❔ Historical Background of Dart
+// 💡=>Dart emerged from Google as a response to the challenges faced by web developers, particularly in the realm of complex web applications. Its development began in October 2011, with the first technical preview released in October 2011 and the first stable release in November 2013.
+// The language was designed to address the shortcomings of JavaScript for large-scale web development, aiming to offer better performance, tools, and developer experience. Dart was envisioned as a language suitable for building structured, scalable, and maintainable web applications.
+// One of the key motivations behind Dart's creation was to provide a language that could be both familiar to existing developers and conducive to building modern web applications. It introduced features like optional typing, classes, and a more traditional object-oriented programming model compared to JavaScript.
+// Initially, Dart aimed to be a replacement for JavaScript in web browsers, but it faced resistance from other browser vendors. Consequently, Google shifted its focus towards using Dart for client-side development with the help of a compiler that translates Dart code into JavaScript. This approach allowed developers to leverage Dart's features while still being compatible with existing web standards.
+// However, Dart found its true breakthrough when Google introduced Flutter in 2017. Flutter, a UI toolkit for building natively compiled applications for mobile, web, and desktop platforms, adopted Dart as its primary language. This decision significantly boosted Dart's popularity and adoption, as developers embraced it for cross-platform app development.
+// Over time, Dart has evolved with new features, improvements, and optimizations, further solidifying its position as a versatile language for web, server, and mobile development. Its integration with Flutter has cemented its relevance in the ever-expanding ecosystem of modern application development.
+
+
+// ❔ What are the Key Features of Dart
+// 💡=>The Key Features of Dart are:
+// 1. Optional Typing: Dart supports both static and dynamic typing, allowing developers to choose whether to explicitly specify variable types or let the compiler infer them.
+// 2. Strong Typing System: Dart's strong typing system helps catch errors at compile-time, leading to more robust and reliable code.
+// 3. Asynchronous Programming: Dart provides built-in support for asynchronous programming using async/await syntax, making it easier to write code that handles asynchronous operations like I/O or network requests.
+// 4. Object-Oriented Programming: Dart is an object-oriented language with support for classes, inheritance, mixins, and interfaces, allowing developers to organize code into reusable and maintainable components.
+// 5. Isolates: Dart uses isolates for concurrency, enabling developers to run code concurrently without shared memory, reducing the risk of race conditions and making it easier to write parallel programs.
+// 6. Garbage Collection: Dart includes automatic memory management through garbage collection, freeing developers from manual memory management tasks and reducing the risk of memory leaks.
+// 7. Standard Libraries: Dart comes with a rich set of standard libraries for common tasks such as file I/O, networking, parsing JSON, and working with collections, enabling developers to build applications more efficiently.
+// 8. Interoperability: Dart supports interoperability with JavaScript, allowing developers to reuse existing JavaScript code and libraries in Dart projects and vice versa.
+// 9. Tooling: Dart provides a comprehensive set of developer tools, including a package manager (pub), an integrated development environment (IDE) with features like code completion and debugging, and a static analyzer for detecting potential issues in code.
+// 10. Cross-Platform Development: With the Flutter framework, Dart enables developers to build high-quality, natively compiled applications for mobile, web, and desktop platforms from a single codebase, making it easier to create and maintain cross-platform apps.
+
+
+// ❔ The Learning Rescources for Dart
+// 💡=>Dart could be learned by Documentation,Books,PDF's,Video Tutorials and via Paid Courses.
+//  So here are the links below:
+//  1)Documentation:
+//    Offical Website=https://dart.dev/
+//    GeeksforGeeks=https://www.geeksforgeeks.org/dart-tutorial/
+//    Javatpoint=https://www.javatpoint.com/dart-programming
+//    Darttutorial=https://dart-tutorial.com/
+//
+//  2)Books:
+//    The Dart Programming Language 1st Edition by Gilad Bracha=https://www.amazon.com/Dart-Programming-Language-Gilad-Bracha/dp/0321927702
+//    Dart zero to hero - 2023=https://www.amazon.com/Dart-zero-hero-Mastering-Language-ebook/dp/B0CJ874DS9
+//
+//  3)PDF's:
+//    The Dart Programming Language 1st Edition by Gilad Bracha=https://ptgmedia.pearsoncmg.com/images/9780321927705/samplepages/9780321927705.pdf
+//    
+//  4)Video Tutorials:
+//    FreeCodeCamp=https://www.youtube.com/watch?v=Ej_Pcr4uC2Q
+//    WsCubeTech=https://www.youtube.com/watch?v=71xacArT_70
+//    ProgrammingWithVishal=https://www.youtube.com/playlist?list=PLWCLxMult9xeeY5NDx4JIWqEnXnlG3fKc
+//    AkshitMadan=https://www.youtube.com/playlist?list=PL9n0l8rSshSlpNWvY4eu_5Oq8l4MRvppT
+//    MahdiMirzadeh=https://www.youtube.com/watch?v=c-ydMfJNfyY
+//    GiraffeAcademy=https://youtube.com/watch?v=5xlVP04905w
+//
+//  5)Paid Courses:
+//    Udemy=https://www.udemy.com/course/complete-dart-guide/?couponCode=OF52424
+//    Udemy=https://www.udemy.com/course/dart-learning-guide/?couponCode=OF52424
+//
---
+++ README.md
@@ -0,0 +1,39 @@
+# Dart
+I this Repository i will upload my Dart basic to advanced tasks :) 
+<br>  
+
+Developer: Ahmed Irfan
+
+![dart](https://github.com/Ahmedirfan786/Dart/assets/129369417/8ccbf0cf-ffb5-49ea-9b1d-0e3d4bffe105)
+
+
+Dart
+An approachable, portable, and productive language for high-quality apps on any platform
+<br>
+Dart is:
+
+Approachable: Develop with a strongly typed programming language that is consistent, concise, and offers modern language features like null safety and patterns.
+
+Portable: Compile to ARM, x64, or RISC-V machine code for mobile, desktop, and backend. Compile to JavaScript or WebAssembly for the web.
+
+Productive: Make changes iteratively: use hot reload to see the result instantly in your running app. Diagnose app issues using DevTools.
+
+Dart's flexible compiler technology lets you run Dart code in different ways, depending on your target platform and goals:
+
+Dart Native: For programs targeting devices (mobile, desktop, server, and more), Dart Native includes both a Dart VM with JIT (just-in-time) compilation and an AOT (ahead-of-time) compiler for producing machine code.
+
+DartW: For programs targeting the web, Dart Web includes both a development time compiler (dartdevc) and a production time compiler (dart2js).
+<br>
+License & patents
+Dart is free and open source.
+
+See LICENSE and PATENT_GRANT.
+
+<br>
+Using Dart
+Visit dart.dev to learn more about the language, tools, and to find codelabs.
+
+Browse pub.dev for more packages and libraries contributed by the community and the Dart team.
+
+Our API reference documentation is published at api.dart.dev, based on the stable release. (We also publish docs from our beta and dev channels, as well as from the main branc).<br>
+Security & reportingvulnerabilitieso report potential vulnerabilities, please see our security policy on https://dart.dev/security.
