a5505b79c778fd541f614c9c8fed5ff5e9b1c082
Ahmed Irfan <alphastarx17@gmail.com>
2024-05-27 21:56:33 +0500

    Added Lessons=>lesson2&3

---
+++ Lessons/ls2_Starting_Dart.dart
@@ -0,0 +1,13 @@
+// Starting Dart
+
+void main(){
+  print("Hello World!");
+}
+
+// Code Explanation
+// 1) Main Function Start: The void main() function marks the beginning of the program's execution. It's where the program starts running its instructions.
+// 2) Main Function Requirement: In Dart, every program must have a main function as its entry point. It's the first function the program calls when it begins execution.
+// 3) Block of Code: The curly braces {} enclose a block of code. In Dart, they denote the start and end of such blocks, grouping together related instructions.
+// 4) Printing Output: The print("Hello World!"); statement displays the text "Hello World!" on the screen or console. It's a way to communicate information to the user or developer.
+// 5) Statement End: Each line of code in Dart typically concludes with a semicolon (;). This signals the end of a statement, indicating to the Dart compiler where one instruction ends and another begins.
+// Additional Note: In Dart, you can also start the code without the void keyword, like main() { }. The void keyword indicates that the main function doesn't return a value. In Dart, it's a convention to use void for functions that don't return anything, although it's optional. If you omit void, Dart implicitly considers the function as returning void.
---
+++ Lessons/ls3_Comments.dart
@@ -0,0 +1,16 @@
+// Dart Comments
+
+// Single Line Comment
+// This is a single-line comment in Dart.
+
+
+
+// Multi-line Comment
+/*
+What Are Comments?🤔
+Comments in programming are annotations within the code that are ignored by the compiler or interpreter. They are used to provide explanations, notes, or documentation about the code, helping developers understand its purpose, behavior, or implementation details. Comments are not executed as part of the program and are solely meant for human readers.
+
+Types of Comments
+1) Single Line Comment 
+2) Multi-line Comment
+*/
