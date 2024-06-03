1f1049c56d16cb7d077d474abc51699459ab7449
Ahmed Irfan <alphastarx17@gmail.com>
2024-06-03 21:39:11 +0500

    Added Basic-Tasks=>Task1&2

---
+++ Basic-Tasks/Task1_String_Interpolation.dart
@@ -0,0 +1,22 @@
+void main(){
+  var firstname="Ahmed";
+  var lastname="Irfan";
+  var age=20;
+  var gender="Male";
+
+
+  print("My name is $firstname $lastname and I am $age years old!");
+  print("\n I am a $gender");
+}
+
+// The main() function is the entry point of the program. It's where the execution of the program starts.
+
+// Four variables are defined: firstname, lastname, age, and gender. These variables store information about a person's name, age, and gender.
+
+// The values assigned to these variables are: "Ahmed" for firstname, "Irfan" for lastname, 20 for age, and "Male" for gender.
+
+// The print() function is used to output messages to the console. In the first print() statement, a message is printed that includes the full name and age interpolated using the variables firstname, lastname, and age.
+
+// In the second print() statement, a message is printed that includes the gender interpolated using the variable gender.
+
+// \n in the second print() statement adds a new line for better formatting.
---
+++ Basic-Tasks/Task2_Fruits_Print.dart
@@ -0,0 +1,18 @@
+void main(){
+  var fruits=["Apple","Banana","Coconut","Dates","Elderberry","Fig","Guava","Honeydew","IceCreamBean","Jackfruit","Kiwi","Lemon","Mango","Nectarine","Orange","PineApple","Quince","Raspberry","Strawberry","Tangerine","Uglifruit","VineyardPeach","WaterMelon","Xigua","Yuzu","Zucchini"];
+  
+  for(var i=0; i< fruits.length; i++){
+    print(fruits[i]);
+  }
+}
+
+
+// The main() function is the entry point of the program.
+
+// A list variable named fruits is defined, containing a list of fruit names. Each fruit name is a string enclosed in double quotes.
+
+// The for loop iterates over each element of the fruits array. It initializes the loop variable i to 0, and the loop continues as long as i is less than the length of the fruits array (fruits.length). After each iteration, i is incremented by 1.
+
+// Inside the loop, fruits[i] is used to access the element at index i of the fruits array, and print() is used to print each fruit name to the console.
+
+// So, when the program is executed, it will print each fruit name in the fruits array, one by one, to the console.
