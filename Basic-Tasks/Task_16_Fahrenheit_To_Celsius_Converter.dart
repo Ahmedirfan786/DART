b55c7490774cfa78f3af242829422f693797d30b
Ahmed Irfan <alphastarx17@gmail.com>
2024-06-09 23:23:39 +0500

    Added Basic-Tasks=>Task16

---
+++ Basic-Tasks/Task_16_Fahrenheit_To_Celsius_Converter.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+void main(){
+  // °C = (°F - 32) × 5/9
+  double? Fahrenheit=double.parse(stdin.readLineSync()!);
+
+  double Celsius=(Fahrenheit -32)*5/9;
+
+  print("$Fahrenheit °F ====> ${Celsius.toStringAsFixed(2)} °C");
+
+
+}
+
+
+// Explanation:
+// The main() function serves as the entry point of the Dart program.
+// It prompts the user to input the temperature in Fahrenheit.
+// These values are then read and parsed as a double.
+// After that, the program calculates the temperature in Celsius using the provided formula.
+// Finally, it prints out the original temperature in Fahrenheit and the converted temperature in Celsius.
+// Import statement for 'dart:io' is used to enable interaction with input/output streams.
