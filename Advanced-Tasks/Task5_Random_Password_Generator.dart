35d587f0c14e0a78f1e5650a31ff5fd988a6a65a
Ahmed Irfan <alphastarx17@gmail.com>
2024-06-25 22:05:55 +0500

    Added Advanced-Tasks=>Task6

--- Advanced-Tasks/Task5_Random_Password_Generator.dart
+++ Advanced-Tasks/Task5_Random_Password_Generator.dart
@@ -23,3 +23,13 @@ String generatePassword(int length) {
   final random = Random();
   return List.generate(length, (index) => chars[random.nextInt(chars.length)]).join();
 }
+
+
+// Explanation:
+// The main() function serves as the entry point of the Dart program.
+// It prompts the user to enter the desired length of the password.
+// The input length is read from the standard input (keyboard) as a String.
+// It attempts to parse the input String into an integer using int.parse().
+// If successful, it calls generatePassword() to generate a password of the specified length.
+// The generated password is then printed to the console.
+// If parsing fails or the input is invalid, an error message is printed indicating invalid input.
---
+++ Advanced-Tasks/Task6_Text_To_MorseCode_Converter.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+
+void main() {
+  print('Enter a message to translate to Morse code:');
+  String input = stdin.readLineSync().toLowerCase();
+
+  String morseCode = textToMorse(input);
+  print('Message in Morse code: $morseCode');
+}
+
+String textToMorse(String text) {
+  Map<String, String> morseCodeMap = {
+    'a': '.-', 'b': '-...', 'c': '-.-.', 'd': '-..', 'e': '.', 'f': '..-.', 
+    'g': '--.', 'h': '....', 'i': '..', 'j': '.---', 'k': '-.-', 'l': '.-..', 
+    'm': '--', 'n': '-.', 'o': '---', 'p': '.--.', 'q': '--.-', 'r': '.-.', 
+    's': '...', 't': '-', 'u': '..-', 'v': '...-', 'w': '.--', 'x': '-..-', 
+    'y': '-.--', 'z': '--..', '0': '-----', '1': '.----', '2': '..---', 
+    '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', 
+    '8': '---..', '9': '----.', ' ': '/'
+  };
+
+  List<String> morseCodeList = [];
+  for (int i = 0; i < text.length; i++) {
+    String char = text[i];
+    String morseChar = morseCodeMap[char] ?? '';
+    morseCodeList.add(morseChar);
+  }
+  return morseCodeList.join(' ');
+}
+
+
+// Explanation:
+// The textToMorse function converts a given text message into Morse code.
+// It uses a predefined map (morseCodeMap) where each key represents a character
+// (including letters, digits, and space) and the corresponding value is its Morse code representation.
+// Each character in the input text is processed individually to retrieve its Morse code
+// from the morseCodeMap, appending each Morse code representation to morseCodeList.
+// Finally, morseCodeList is joined into a single string separated by spaces and returned as the Morse code representation of the input text.
